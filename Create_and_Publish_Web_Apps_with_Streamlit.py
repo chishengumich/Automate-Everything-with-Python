0b7edb65b0ae529e3bd5c15a16ae44623a24e078
chishengumich <54919554+chishengumich@users.noreply.github.com>
2022-07-15 23:41:36 -0400

    Update Create_and_Publish_Web_Apps_with_Streamlit.py
    Add pandas table

--- Create_and_Publish_Web_Apps_with_Streamlit.py
+++ Create_and_Publish_Web_Apps_with_Streamlit.py
@@ -1,9 +1,18 @@
 import streamlit as st
+import pandas 
 
+
+data = {
+    'Series_1':[1,2,3,4,7], 
+    'Series_2':[10,20,30,100,250]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('Out First Streamlit App')
 st.subheader('Introducing Streamlit in Antomate Evertthing with python')
-
 st.write('''This is our first web App. ''')
 
+
+st.write(df)
 
