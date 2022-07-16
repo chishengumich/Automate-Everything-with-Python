4e2cdeb21d8e2ecaed4a46fefec6287b8bb9aec9
chishengumich <54919554+chishengumich@users.noreply.github.com>
2022-07-15 23:35:04 -0400

    Add files via upload

---
+++ Create_and_Publish_Web_Apps_with_Streamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.title('Out First Streamlit App')
+st.subheader('Introducing Streamlit in Antomate Evertthing with python')
+
+st.write('''This is our first web App. ''')
+
+
